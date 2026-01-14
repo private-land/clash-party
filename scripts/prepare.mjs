03fb7ca8085c70a5cc360230fd71a700f2ce46d8
huzibaca <masterorigin91@gmail.com>
2026-01-15 00:35:32 +0800

    fix: update mihomo-smart binary names to v3-go124

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -51,12 +51,12 @@ const MIHOMO_SMART_URL_PREFIX = `https://github.com/xflash-panda/mihomo/releases
 let MIHOMO_SMART_VERSION
 
 const MIHOMO_SMART_MAP = {
-  'win32-x64': 'mihomo-windows-amd64-v2-go120',
-  'win32-ia32': 'mihomo-windows-386-go120',
+  'win32-x64': 'mihomo-windows-amd64-v3-go124',
+  'win32-ia32': 'mihomo-windows-386-go124',
   'win32-arm64': 'mihomo-windows-arm64',
-  'darwin-x64': 'mihomo-darwin-amd64-v2-go120',
+  'darwin-x64': 'mihomo-darwin-amd64-v3-go124',
   'darwin-arm64': 'mihomo-darwin-arm64',
-  'linux-x64': 'mihomo-linux-amd64-v2-go120',
+  'linux-x64': 'mihomo-linux-amd64-v3',
   'linux-arm64': 'mihomo-linux-arm64'
 }
 
