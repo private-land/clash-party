766651db1377fe8783b43e2cd7c71ed603468213
huzibaca <masterorigin91@gmail.com>
2026-03-14 19:04:10 +0800

    fix: update default User-Agent to mihomo.panda for client identification

--- src/main/config/profile.ts
+++ src/main/config/profile.ts
@@ -252,7 +252,7 @@ export async function createProfile(item: Partial<IProfileItem>): Promise<IProfi
   const baseOptions: Omit<FetchOptions, 'useProxy' | 'timeout'> = {
     url: item.url,
     mixedPort,
-    userAgent: userAgent || `mihomo.party/v${app.getVersion()} (clash.meta)`,
+    userAgent: userAgent || `mihomo.panda/v${app.getVersion()} (clash.meta)`,
     authToken: item.authToken,
     substore: newItem.substore || false
   }
